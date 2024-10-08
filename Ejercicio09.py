6396d18c8d5182a63f9c2a3d7750d7b00fcefcd4
xgarciaech1 <xgarciaech@educacion.navarra.es>
2024-10-08 18:30:15 +0200

    Create Ejercicio09.py

---
+++ Ejercicio09.py
@@ -0,0 +1,10 @@
+#Escribir un programa que pregunte al usuario una cantidad a invertir,
+#el interés anual y el número de años,
+#y muestre por pantalla el capital obtenido en la inversión.
+cantida = input("Cantidad a invertir: ")
+interes = input("Interes anual: ")
+años = input("El numero de años que quieres invertir: ")
+x = float(cantida)
+y = float(interes)
+u = float(años
+resultado_final = print(x * (1 + (u * y) / 100))
