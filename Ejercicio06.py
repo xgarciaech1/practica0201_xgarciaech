f90fb2d07f703387dfe14a7ac528295ea96e2cdd
xgarciaech1 <xgarciaech@educacion.navarra.es>
2024-10-08 17:08:26 +0200

    Update Ejercicio06.py

--- Ejercicio06.py
+++ Ejercicio06.py
@@ -1,1 +1,10 @@
+<<<<<<< HEAD
 print(5 * 6)
+=======
+#Escribir un programa que lea un entero positivo, introducido por el usuario
+#y después muestre en pantalla la suma de todos los enteros desde 1 hasta.
+#La suma de los  primeros enteros positivos puede ser calculada
+# de la siguiente forma: 
+n = int(input("Dime un numero:"))
+print(int((n * (n + 1))/ 2))
+>>>>>>> 3e39aa6468000788bff3d33b63c5e110ca43c53f
