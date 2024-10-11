8da1fccad43c2630f998a0262915a5c0e120ceb7
xgarciaech1 <xgarciaech@educacion.navarra.es>
2024-10-11 13:42:05 +0200

    Update Ejercicio08.py

--- Ejercicio08.py
+++ Ejercicio08.py
@@ -2,6 +2,9 @@
 #pantalla la <n> entre <m> da un cociente <c> y un resto <r> donde <n> y <m>
 #son los números introducidos por el usuario, y <c> y <r> son el cociente 
 #y el resto de la división entera respectivamente.
-n = input("Escribir un nimero entero:")
-m = input("Escribe un numero entero: ")
-print(n // m)
+n = int(input("Escribir un nimero entero:"))
+m = int(input("Escribe un numero entero: "))
+c = (n % m)
+r = (n // m)
+print("El coeficiente c serai:", c)
+print("El coeficiente r seria:", r)
